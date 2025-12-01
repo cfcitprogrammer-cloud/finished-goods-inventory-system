c1d780c60ef57a17c5f790d0beabb89cada057e9
cfcitprogrammer-cloud <cfcitprogrammer@gmail.com>
2025-12-01 16:46:07 +0800

    prepare to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Nav from "./components/Nav";
-import { Routes, Route } from "react-router-dom";
 import MasterDB from "./components/MasterDB";
 import FinishedGoods from "./components/FinishedGoods";
 
@@ -11,6 +10,8 @@ function App() {
 
       <div className="max-w-[1000px] mx-auto p-4">
         <Routes>
+          {/* Redirect from "/" to "/master-db" */}
+          <Route path="/" element={<Navigate to="/finished-goods" replace />} />
           <Route path="/master-db" element={<MasterDB />} />
           <Route path="/finished-goods" element={<FinishedGoods />} />
         </Routes>
