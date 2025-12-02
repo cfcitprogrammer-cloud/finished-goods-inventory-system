e9ab5b741e35e131dd70b76de20c0ebd92c5e894
cfcitprogrammer-cloud <cfcitprogrammer@gmail.com>
2025-12-02 09:58:04 +0800

    updated telegram bot

--- src/data/links.js
+++ src/data/links.js
@@ -1,4 +1,4 @@
 export const BASE_API_URL =
-  "https://script.google.com/macros/s/AKfycbwQi5wlWG-3YfAuEdagwuuoCdoJs-mVJYNsLbN_5OJHBGr2oGSS4BIZMol4DKBBYcc/exec";
+  "https://script.google.com/macros/s/AKfycbytco7JVHXbesqXK7zouDKJQtjBa4Ftvy2Sq91iD1M4CEcx9dEp2szVn7v0qmGX6VI/exec";
 export const GET_ACTION_FETCH_ALL_SHEET = "?action=fetch-all-sheet";
 export const GET_ACTION_FETCH_MASTER_DB = "?action=fetch-master-db";
