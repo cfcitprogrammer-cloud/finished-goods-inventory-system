e28c7aeda4548a39510f465c8cf77f01a84ca6b8
cfcitprogrammer-cloud <cfcitprogrammer@gmail.com>
2025-12-04 09:06:09 +0800

    updated link

--- src/data/links.js
+++ src/data/links.js
@@ -1,4 +1,4 @@
 export const BASE_API_URL =
-  "https://script.google.com/macros/s/AKfycbytco7JVHXbesqXK7zouDKJQtjBa4Ftvy2Sq91iD1M4CEcx9dEp2szVn7v0qmGX6VI/exec";
+  "https://script.google.com/macros/s/AKfycbzCbJevIzhwMLGLpt9xOKg2LJ1Rurxi5XMGqBfn6Zseti0h2uo3WPASDbN5EmwiFrk/exec";
 export const GET_ACTION_FETCH_ALL_SHEET = "?action=fetch-all-sheet";
 export const GET_ACTION_FETCH_MASTER_DB = "?action=fetch-master-db";
